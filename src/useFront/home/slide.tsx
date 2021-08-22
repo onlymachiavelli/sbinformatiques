3fe339537948d8ef6a96d6198495ce988dd768cf
onlymachiavelli <tmakaveli643@gmail.com>
2021-08-22 22:00:33 +0100

    Navbar

--- src/useFront/home/slide.tsx
+++ src/useFront/home/slide.tsx
@@ -1,10 +1,21 @@
 import React, {} from 'react'
+import Link from 'next/link'
 import styles from './../../../styles/Home.module.css'
 const Slide = () =>{
     return(
         <div className={styles.slide_container}>
             <header className={styles.header}>
-                <nav className={styles.navbar} ></nav>
+                <nav className={styles.navbar} >
+                    <div className={styles.logo}>
+                        <Link href="/">SBINFORMATIQUES</Link>
+                    </div>
+                    <nav className={styles.nav} >
+                    <Link href="/">Acceuil</Link>
+                    <Link href="/">Services</Link>
+                    <Link href="/">Tarif</Link>
+                    <Link href="/">A Propos</Link>
+                    </nav>
+                </nav>
             </header>
         </div>
     )
