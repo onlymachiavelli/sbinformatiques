42579ccbab317ac162590cbcf51b6b75734630df
onlymachiavelli <tmakaveli643@gmail.com>
2021-08-22 21:52:17 +0100

    Slide Nav

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import Head from "next/head"
 import Image from "next/image"
 import styles from "../styles/Home.module.css"
+import Slide from './../src/useFront/home/slide'
 
 const SBI = () => {
-  return <div>hain</div>
+  return (
+    <main>
+        <Slide/>
+    </main>
+    )
 }
 export default SBI
---
+++ src/useFront/home/slide.tsx
@@ -0,0 +1,13 @@
+import React, {} from 'react'
+import styles from './../../../styles/Home.module.css'
+const Slide = () =>{
+    return(
+        <div className={styles.slide_container}>
+            <header className={styles.header}>
+                <nav className={styles.navbar} ></nav>
+            </header>
+        </div>
+    )
+}
+
+export default Slide
