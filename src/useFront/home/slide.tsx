2e8ef6bec0b1d55612a8e12468d650f7995072e9
onlymachiavelli <tmakaveli643@gmail.com>
2021-08-23 16:10:54 +0100

    NV BTN RES

--- src/useFront/home/slide.tsx
+++ src/useFront/home/slide.tsx
@@ -10,20 +10,25 @@ const Slide = () =>{
                     <div className={styles.logo}>
                         <Link href="/">SBINFORMATIQUES</Link>
                     </div>
+                    <div className={styles.resbtn}>
+                        <button>
+                            <Bars/>
+                        </button>
+                    </div>
                     <nav className={styles.nav} >
-                    <Link href="/">Acceuil</Link>
-                    <Link href="/">Services</Link>
-                    <Link href="/">Tarif</Link>
-                    <Link href="/">A Propos</Link>
+                        <Link href="/">Acceuil</Link>
+                        <Link href="/">Services</Link>
+                        <Link href="/">Tarif</Link>
+                        <Link href="/">A Propos</Link>
                     </nav>
-                <div className={styles.contact}>
-                    <Link href="/">
-                        <a className="contacta" style={{backgroundColor:"var(--Blue)"}}>ContactezNous</a>
-                    </Link>
-                    <Link href="/">
-                        <a className="calla" style={{backgroundColor:"#3B712E"}}>AppelNous</a>
-                    </Link>
-                </div>
+                    <div className={styles.contact}>
+                        <Link href="/">
+                            <a className="contacta" style={{backgroundColor:"var(--Blue)"}}>ContactezNous</a>
+                        </Link>
+                        <Link href="/">
+                            <a className="calla" style={{backgroundColor:"#3B712E"}}>AppelNous</a>
+                        </Link>
+                    </div>
                 </nav>
             </header>
         </div>
