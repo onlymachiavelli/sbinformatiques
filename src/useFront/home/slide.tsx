efda64439a51ac06614f6ba204ad7d453c978cc9
onlymachiavelli <tmakaveli643@gmail.com>
2021-08-23 16:23:58 +0100

    res window

--- src/useFront/home/slide.tsx
+++ src/useFront/home/slide.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import styles from './../../../styles/Home.module.css'
 import {Bars} from './../SVG'
 const Slide = () =>{
+    const [Nav, setNav] = useState("block")
     return(
         <div className={styles.slide_container}>
             <header className={styles.header}>
@@ -11,17 +12,19 @@ const Slide = () =>{
                         <Link href="/">SBINFORMATIQUES</Link>
                     </div>
                     <div className={styles.resbtn}>
-                        <button>
+                        <button onClick={()=>{
+                            setNav(Nav === "none" ? "block" : "none")
+                        }}>
                             <Bars/>
                         </button>
                     </div>
-                    <nav className={styles.nav} >
+                    <nav className={styles.nav} style={{display:Nav}} >
                         <Link href="/">Acceuil</Link>
                         <Link href="/">Services</Link>
                         <Link href="/">Tarif</Link>
                         <Link href="/">A Propos</Link>
                     </nav>
-                    <div className={styles.contact}>
+                    <div className={styles.contact} style={{display:Nav}}>
                         <Link href="/">
                             <a className="contacta" style={{backgroundColor:"var(--Blue)"}}>ContactezNous</a>
                         </Link>
