6886d2729a009a676774439265408e63a2b0a755
onlymachiavelli <tmakaveli643@gmail.com>
2021-08-24 02:21:05 +0100

    cards

--- src/useFront/home/obj.tsx
+++ src/useFront/home/obj.tsx
@@ -1,14 +1,32 @@
 import React from 'react'
 import styles from './../../../styles/Home.module.css'
-import { } from './../SVG'
+import { Computer, Btime, Settings } from './../SVG'
 
 const Obj = () => {
 
     return (
         <div className={styles.obj_container}>
-            <div className={styles.card}></div>
-            <div className={styles.card}></div>
-            <div className={styles.card}></div>
+            <div className={styles.card}>
+                <Btime />
+                <p id={styles.card_title}>Support performant et réactif</p>
+                <p id={styles.card_txt}>
+                    Proservices Informatique assure un support d’assistance informatique 7 jours sur 7 de 8 heures à 22 heures, en vous accueillant par e-mail ou ticket d’assistance informatique pour vous apporter des solutions
+                </p>
+            </div>
+            <div className={styles.card}>
+                <Settings />
+                <p id={styles.card_title}>Intervention à distance et à domicile</p>
+                <p id={styles.card_txt}>
+                    Proservices Informatique assure un support d’assistance informatique 7 jours sur 7 de 8 heures à 22 heures, en vous accueillant par e-mail ou ticket d’assistance informatique pour vous apporter des solutions
+                </p>
+            </div>
+            <div className={styles.card}>
+                <Computer />
+                <p id={styles.card_title}>Solutions informatique</p>
+                <p id={styles.card_txt}>
+                    Proservices Informatique assure un support d’assistance informatique 7 jours sur 7 de 8 heures à 22 heures, en vous accueillant par e-mail ou ticket d’assistance informatique pour vous apporter des solutions
+                </p>
+            </div>
         </div>
     )
 }
