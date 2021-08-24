d4604a83c0b465ec99110e0d7d2fe479db3d3c66
onlymachiavelli <tmakaveli643@gmail.com>
2021-08-24 21:06:40 +0100

    Cards link

--- src/useFront/home/services.tsx
+++ src/useFront/home/services.tsx
@@ -1,6 +1,8 @@
+import Link from 'next/link'
 import React from 'react'
 import styles from './../../../styles/Home.module.css'
 import { Mant, Depann, Assis } from './../SVG'
+
 const Services = () =>{
     return(
        <main style={{backgroundColor:"#ededed", width:"100%", height:"auto"}}>
@@ -9,14 +11,23 @@ const Services = () =>{
                 <div className={styles.card}>
                     <Depann/>
                     <p id={styles.card_title}>Depannage Informatique</p>
+                    <p id={styles.card_txt}>Proservices Informatique assure un support d’assistance...</p>
+                    <br/><br/>
+                    <Link href="">Devis</Link>
                 </div>
                 <div className={styles.card}>
                     <Assis/>
                     <p id={styles.card_title}>Assistance Informatique</p>
+                    <p id={styles.card_txt}>Proservices Informatique assure un support d’assistance...</p>
+                    <br/><br/>
+                    <Link href="">Devis</Link>
                 </div>
                 <div className={styles.card}>
                     <Mant/>
                     <p id={styles.card_title}>Maintenance Informatique</p>
+                    <p id={styles.card_txt}>Proservices Informatique assure un support d’assistance...</p>
+                    <br/><br/>
+                    <Link href="">Devis</Link>
                 </div>
             </div>
         </main>
