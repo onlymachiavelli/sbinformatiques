77bd5cbf3d1d66af3f1e5290f8bde5c57ed354eb
onlymachiavelli <tmakaveli643@gmail.com>
2021-08-24 02:08:31 +0100

    Scroll bar + obj bloc

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,13 @@ import Head from "next/head"
 import Image from "next/image"
 import styles from "../styles/Home.module.css"
 import Slide from './../src/useFront/home/slide'
-
+import Obj from './../src/useFront/home/obj'
 const SBI = () => {
   return (
     <main>
-        <Slide/>
+      <Slide />
+      <Obj />
     </main>
-    )
+  )
 }
 export default SBI
---
+++ src/useFront/home/obj.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './../../../styles/Home.module.css'
+import { } from './../SVG'
+
+const Obj = () => {
+
+    return (
+        <div className={styles.obj_container}>
+            <div className={styles.card}></div>
+            <div className={styles.card}></div>
+            <div className={styles.card}></div>
+        </div>
+    )
+}
+export default Obj
