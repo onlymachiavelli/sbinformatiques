ef6612ce37e05ed968bc15f170b5cc68475bca0d
onlymachiavelli <tmakaveli643@gmail.com>
2021-08-26 18:09:58 +0100

    vplus link serv

--- src/useFront/home/services.tsx
+++ src/useFront/home/services.tsx
@@ -3,32 +3,37 @@ import React from 'react'
 import styles from './../../../styles/Home.module.css'
 import { Mant, Depann, Assis } from './../SVG'
 
-const Services = () =>{
-    return(
-       <main style={{backgroundColor:"#ededed", width:"100%", height:"auto"}}>
+const Services = () => {
+    return (
+        <main style={{ backgroundColor: "#ededed", width: "100%", height: "auto" }}>
             <p id={styles.tit}>Nos Services</p>
             <div className={styles.serv_container}>
                 <div className={styles.card}>
-                    <Depann/>
+                    <Depann />
                     <p id={styles.card_title}>Depannage Informatique</p>
                     <p id={styles.card_txt}>Proservices Informatique assure un support d’assistance...</p>
-                    <br/><br/>
+                    <br /><br />
                     <Link href="">Devis</Link>
                 </div>
                 <div className={styles.card}>
-                    <Assis/>
+                    <Assis />
                     <p id={styles.card_title}>Assistance Informatique</p>
                     <p id={styles.card_txt}>Proservices Informatique assure un support d’assistance...</p>
-                    <br/><br/>
+                    <br /><br />
                     <Link href="">Devis</Link>
                 </div>
                 <div className={styles.card}>
-                    <Mant/>
+                    <Mant />
                     <p id={styles.card_title}>Maintenance Informatique</p>
                     <p id={styles.card_txt}>Proservices Informatique assure un support d’assistance...</p>
-                    <br/><br/>
+                    <br /><br />
                     <Link href="">Devis</Link>
                 </div>
+            </div>
+            <div style={{ width: "100%", height: "auto", textAlign: "center", marginBottom: "60px", marginTop: "20px" }}>
+                <Link href="/services">
+                    <a id={styles.vplus}> Voir Plus</a>
+                </Link>
             </div>
         </main>
     )
