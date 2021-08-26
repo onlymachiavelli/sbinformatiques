cc7d0a774c722d0ec931a9588236eda242f4dadb
onlymachiavelli <tmakaveli643@gmail.com>
2021-08-26 21:27:29 +0100

    Geo map

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,21 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import Head from "next/head"
 import Image from "next/image"
 import styles from "../styles/Home.module.css"
 import Slide from './../src/useFront/home/slide'
 import Obj from './../src/useFront/home/obj'
-import Services  from "../src/useFront/home/services"
+import Services from "../src/useFront/home/services"
+import Gmap from './../src/useFront/home/map'
 const SBI = () => {
-  useEffect(()=>{
+  useEffect(() => {
     document.title = "SBI | Acceuil"
   })
   return (
     <main>
       <Slide />
       <Obj />
-      <Services/>
+      <Services />
+      <Gmap />
     </main>
   )
 }
---
+++ src/useFront/home/map.tsx
@@ -0,0 +1,10 @@
+import React, { } from 'react'
+
+const Gmap = () => {
+
+    return (
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12769.97469064152!2d10.2638026!3d36.8546006!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3a8849463d743622!2sSBI%20(Solution%20Bureautique%20Informatique)!5e0!3m2!1sen!2stn!4v1630009000087!5m2!1sen!2stn" width="100%" height="500px" style={{ border: 0 }} loading="lazy"></iframe>
+    )
+}
+
+export default Gmap
