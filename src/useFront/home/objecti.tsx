e26a54ad80b5a3db935e4be7bbbab04195cf83f5
onlymachiavelli <tmakaveli643@gmail.com>
2021-08-28 17:51:57 +0100

    Objectives

--- src/useFront/home/objecti.tsx
+++ src/useFront/home/objecti.tsx
@@ -9,6 +9,36 @@ const Ob = () => {
                 <div className={styles.ob_img}>
                     <Face />
                 </div>
+                <div>
+                    <p id={styles.o_title}>Satisfaction de nos clients</p>
+                    <p id={styles.o_d}>
+                        Yedhom f z4536 Informatique assure un support d’assistance informatique 7 jours sur 7 de 8 heures à 22 heures, en vous accueillant par e-mail ou ticket d’assistance informatique pour vous apporter des solutions
+                    </p>
+                </div>
+            </div>
+
+            <div className={styles.ob_c}>
+                <div className={styles.ob_img}>
+                    <Face />
+                </div>
+                <div>
+                    <p id={styles.o_title}>Satisfaction de nos clients</p>
+                    <p id={styles.o_d}>
+                        Yedhom f z4536 Informatique assure un support d’assistance informatique 7 jours sur 7 de 8 heures à 22 heures, en vous accueillant par e-mail ou ticket d’assistance informatique pour vous apporter des solutions
+                    </p>
+                </div>
+            </div>
+
+            <div className={styles.ob_c}>
+                <div className={styles.ob_img}>
+                    <Face />
+                </div>
+                <div>
+                    <p id={styles.o_title}>Satisfaction de nos clients</p>
+                    <p id={styles.o_d}>
+                        Yedhom f z4536 Informatique assure un support d’assistance informatique 7 jours sur 7 de 8 heures à 22 heures, en vous accueillant par e-mail ou ticket d’assistance informatique pour vous apporter des solutions
+                    </p>
+                </div>
             </div>
         </div>
     )
