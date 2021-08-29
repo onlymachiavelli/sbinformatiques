52eab40140578c4679b872773b7dc921ecdc305e
onlymachiavelli <tmakaveli643@gmail.com>
2021-08-29 21:17:52 +0100

    Setting up independent file Navbar

--- src/useFront/home/slide.tsx
+++ src/useFront/home/slide.tsx
@@ -1,44 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 import styles from './../../../styles/Home.module.css'
-import { Bars, Handdown } from './../SVG'
+import { Handdown } from './../SVG'
 import Image from 'next/image'
 import Image1 from './../../../public/image1.png'
+import Navbar from '../navbar'
 const Slide = () => {
-    const [Nav, setNav] = useState("none")
-    const Hash = useEffect(() => {
-        console.log("damn")
-    })
     return (
         <div className={styles.slide_container}>
-            <header className={styles.header}>
-                <nav className={styles.navbar} >
-                    <div className={styles.logo}>
-                        <Link href="/">SBINFORMATIQUES</Link>
-                    </div>
-                    <div className={styles.resbtn}>
-                        <button onClick={() => {
-                            setNav(Nav === "none" ? "block" : "none")
-                        }}>
-                            <Bars />
-                        </button>
-                    </div>
-                    <nav className={styles.nav} style={{ display: Nav }} >
-                        <Link href="/">Acceuil</Link>
-                        <Link href="/">Services</Link>
-                        <Link href="/">Tarif</Link>
-                        <Link href="/">A Propos</Link>
-                    </nav>
-                    <div className={styles.contact} style={{ display: Nav }}>
-                        <Link href="/">
-                            <a className="contacta" style={{ backgroundColor: "var(--Blue)" }}>ContactezNous</a>
-                        </Link>
-                        <Link href="/">
-                            <a className="calla" style={{ backgroundColor: "#3B712E" }}>AppelNous</a>
-                        </Link>
-                    </div>
-                </nav>
-            </header>
+            <Navbar/>
 
             <main className={styles.desc}>
                 <article className={styles.article}>
---
+++ src/useFront/navbar.tsx
@@ -0,0 +1,40 @@
+import React, {useState} from 'react'
+import styles from './../../styles/Home.module.css'
+import Link from 'next/link'
+import {Bars} from './SVG'
+const Navbar = () =>{
+    const [Nav, setNav] = useState("none")
+    return(
+        <header className={styles.header}>
+                <nav className={styles.navbar} >
+                    <div className={styles.logo}>
+                        <Link href="/">SBINFORMATIQUES</Link>
+                    </div>
+                    <div className={styles.resbtn}>
+                        <button onClick={() => {
+                            setNav(Nav === "none" ? "block" : "none")
+                        }}>
+                            <Bars />
+                        </button>
+                    </div>
+                    <nav className={styles.nav} style={{ display: Nav }} >
+                        <Link href="/">Acceuil</Link>
+                        <Link href="/">Services</Link>
+                        <Link href="/">Tarif</Link>
+                        <Link href="/">A Propos</Link>
+                    </nav>
+                    <div className={styles.contact} style={{ display: Nav }}>
+                        <Link href="#contact">
+                            <a className="contacta" style={{ backgroundColor: "var(--Blue)" }}>ContactezNous</a>
+                        </Link>
+                        <Link href="/contact">
+                            <a className="calla" style={{ backgroundColor: "#3B712E" }}>AppelNous</a>
+                        </Link>
+                    </div>
+                </nav>
+            </header>
+    )
+}
+
+
+export default Navbar
