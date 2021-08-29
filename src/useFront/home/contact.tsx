864ae5f0b66b7b8d2ff7134ec65490ca09a8ca74
onlymachiavelli <tmakaveli643@gmail.com>
2021-08-29 18:50:41 +0100

    fgfg

--- src/useFront/home/contact.tsx
+++ src/useFront/home/contact.tsx
@@ -1,6 +1,6 @@
 import React, { } from 'react'
 import styles from './../../../styles/Home.module.css'
-import { } from './../SVG'
+import { User, Mobile, Email } from './../SVG'
 
 const Contact = () => {
     return (
@@ -8,7 +8,31 @@ const Contact = () => {
             <p id={styles.c_title}>Contactez Nous</p>
             <form onSubmit={() => { }}>
                 <div className={styles.input}>
+                    <div style={{ marginTop: "3px", }}>
+                        <User />
+                    </div>
+                    <input type="text" name="" placeholder="Votre Nom Complet" />
+                </div>
 
+                <div className={styles.input}>
+                    <div style={{ marginTop: "3px", }}>
+                        <Mobile />
+                    </div>
+                    <input type="text" name="" placeholder="Votre Nom Complet" />
+                </div>
+
+                <div className={styles.input}>
+                    <div style={{ marginTop: "3px", }}>
+                        <Mobile />
+                    </div>
+                    <input type="text" name="" placeholder="Votre Nom Complet" />
+                </div>
+
+                <div className={styles.input}>
+                    <div style={{ marginTop: "3px", }}>
+                        <Mobile />
+                    </div>
+                    <input type="text" name="" placeholder="Votre Nom Complet" />
                 </div>
             </form>
         </div>
