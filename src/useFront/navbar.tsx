505fa732e621fbdf2ac2eb3033e2a5ff5acfb81c
onlymachiavelli <tmakaveli643@gmail.com>
2021-08-29 21:52:32 +0100

    Res contact area home

--- src/useFront/navbar.tsx
+++ src/useFront/navbar.tsx
@@ -24,7 +24,7 @@ const Navbar = () =>{
                         <Link href="/">A Propos</Link>
                     </nav>
                     <div className={styles.contact} style={{ display: Nav }}>
-                        <Link href="/Cont">
+                        <Link href="/ContactUs/">
                             <a className="contacta" style={{ backgroundColor: "var(--Blue)" }}>ContactezNous</a>
                         </Link>
                         <Link href="/contact">
