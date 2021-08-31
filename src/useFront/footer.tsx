435be223226fe158750d52ce3d4d4bd0666e4cef
onlymachiavelli <tmakaveli643@gmail.com>
2021-08-31 17:27:59 +0100

    Footer

--- src/useFront/footer.tsx
+++ src/useFront/footer.tsx
@@ -9,9 +9,34 @@ const Footer = () => {
                 <b>SBINFORMATIQUES</b>
             </div>
             <div className={styles.footer_btns}>
-                <Link href=""><a >cCommander Un Devis</a></Link>
-                <Link href=""><a >cCommander Un Devis</a></Link>
+                <Link href=""><a style={{background:"var(--Blue)"}} >Commander Un Devis</a></Link>
+                <Link href=""><a style={{background:"#3B712E"}}>Appel Nous</a></Link>
             </div>
+            <div className={styles.social_media}>
+                <Link href="">
+                    <Smail/>
+                </Link>&nbsp;
+                <Link href="">
+                    <Whatsapp/>
+                </Link>&nbsp;
+                <Link href="">
+                    <Messenger/>
+                </Link>&nbsp;
+                <Link href="">  
+                    <Facebook/>
+                </Link>
+                <br/><br/>
+                <Link href="">
+                    <a 
+                        style={{
+                            color:"#fff",
+                            textDecoration:"underline",
+                        }}
+                    >sbinforatiques@crocodile.com</a>
+                </Link>
+            </div>
+            <p id={styles.phonen}>51 920 215 / 70 720 215</p>
+            <div className={styles.copy}></div>
         </footer>
     )
 }
