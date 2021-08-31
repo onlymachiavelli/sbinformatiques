541796dbb7edc36b4a9936c50ccccefeccfdea45
onlymachiavelli <tmakaveli643@gmail.com>
2021-08-31 17:06:51 +0100

    Changing the footer design

--- src/useFront/footer.tsx
+++ src/useFront/footer.tsx
@@ -1,42 +1,16 @@
 import React from 'react'
 import styles from './../../styles/Home.module.css'
 import {Face, Facebook , Messenger, Smail, Whatsapp} from './SVG'
+import Link from 'next/link'
 const Footer = () => {
     return (
         <footer className={styles.footer}>
             <div className={styles.footer_logo}>
-                <p>SBINFORMATIQUES</p>
+                <b>SBINFORMATIQUES</b>
             </div>
-            <div className={styles.footerflex}>
-                <div className={styles.social_info}>
-                    <div className={styles.social}>
-                        <a href="#">
-                                <Smail/>
-                        </a>
-
-                        <a href="#">
-                                <Whatsapp/>
-                        </a>
-
-                        <a href="#">
-                                <Messenger/>
-                        </a>
-
-                        <a href="#">
-                                <Facebook/>
-                        </a>
-                        
-                    </div>
-
-                    
-                </div>
-                <div>
-
-                </div>
-
-                <div>
-                    
-                </div>
+            <div className={styles.footer_btns}>
+                <Link href=""><a >cCommander Un Devis</a></Link>
+                <Link href=""><a >cCommander Un Devis</a></Link>
             </div>
         </footer>
     )
