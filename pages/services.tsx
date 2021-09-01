954b624b12ab4295f4c4bf4f55a21d723850317d
onlymachiavelli <tmakaveli643@gmail.com>
2021-09-01 21:33:00 +0100

    Service infos HTML

--- pages/services.tsx
+++ pages/services.tsx
@@ -1,15 +1,46 @@
-import React, { } from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './../src/useFront/navbar'
 import Footer from './../src/useFront/footer'
 import styles from './../styles/Home.module.css'
+import { Depann, Assis, Mant } from './../src/useFront/SVG'
+
 const Services = () => {
+    useEffect(() => {
+        document.title = "SBI | NOS services"
+    })
     return (
         <div className="">
             <Navbar />
+
             <div className={styles.service_container}>
+                <br /><br /><br /><br />
+                <div className={styles.b_title}>
+                    Nos Services
+                </div>
+                <br />
+                <br />
+
                 <div className={styles.serv_flex}>
                     <div className={styles.service_card}>
+                        <Depann />
+                        <p id={styles.serv_title}>Depannage Informatiques</p>
+                        <p id={styles.serv_description}>
+                            Quel que soit les problèmes informatiques que vous rencontrez avec votre ordinateur, nous analyserons et solutionnerons tous vos soucis informatiques rapidement via une intervention informatique à domicile ou à distance.
+                        </p>
+                    </div>
 
+                    <div className={styles.service_card}>
+                        <Assis />
+                        <p id={styles.serv_title}>Assistance Informatiques</p>
+                        <p id={styles.serv_description}>
+                            Un spécialiste informatique à vos côtés pour tous vos besoins : installation et configuration de matériels informatiques ou logiciels, assembler un ordinateur sur mesure, conception de sites internet WordPress…
+                        </p>
+                    </div>
+
+                    <div className={styles.service_card}>
+                        <Mant />
+                        <p id={styles.serv_title}>Maintenance Infromatiques</p>
+                        <p id={styles.serv_description}>Optez pour la tranquillité en nous confiant le contrôle, l’entretien, l’optimisation et la sécurité de votre ordinateur de bureau ou PC portable. Des performances optimales, sans dysfonctionnements, en toute sérénité.</p>
                     </div>
                 </div>
             </div>
