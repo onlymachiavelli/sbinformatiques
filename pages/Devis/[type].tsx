f1c8eacd20524aacec2bcd1c2358ddb501b759bd
onlymachiavelli <tmakaveli643@gmail.com>
2021-09-05 16:26:40 +0100

    Fixing phonee indicator

--- pages/Devis/[type].tsx
+++ pages/Devis/[type].tsx
@@ -1,18 +1,18 @@
-import React, {} from 'react'
+import React, { } from 'react'
 import { useRouter } from 'next/router'
 
 
-const IsIn = (Route:String)=> {
+const IsIn = (Route: String) => {
     let RoutesArray = []
-    for (let i=0;i<RoutesArray.length;i++)if(RoutesArray[i] === Route) return true
+    for (let i = 0; i < RoutesArray.length; i++)if (RoutesArray[i] === Route) return true
     return false
 }
 
 
-const Devis = () =>{
+const Devis = () => {
     const Route = useRouter()
-    const {type}  = Route.query
-    return(
+    const { type } = Route.query
+    return (
         <div>
             {type}
         </div>
--- src/useFront/navbar.tsx
+++ src/useFront/navbar.tsx
@@ -1,37 +1,37 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import styles from './../../styles/Home.module.css'
 import Link from 'next/link'
-import {Bars} from './SVG'
-const Navbar = () =>{
+import { Bars } from './SVG'
+const Navbar = () => {
     const [Nav, setNav] = useState("none")
-    return(
+    return (
         <header className={styles.header}>
-                <nav className={styles.navbar} >
-                    <div className={styles.logo}>
-                        <Link href="/">SBINFORMATIQUES</Link>
-                    </div>
-                    <div className={styles.resbtn}>
-                        <button onClick={() => {
-                            setNav(Nav === "none" ? "block" : "none")
-                        }}>
-                            <Bars />
-                        </button>
-                    </div>
-                    <nav className={styles.nav} style={{ display: Nav }} >
-                        <Link href="/">Acceuil</Link>
-                        <Link href="/services">Services</Link>
-                        <Link href="/about">A Propos</Link>
-                    </nav>
-                    <div className={styles.contact} style={{ display: Nav }}>
-                        <Link href="/contact">
-                            <a className="contacta" style={{ backgroundColor: "var(--Blue)" }}>ContactezNous</a>
-                        </Link>
-                        <Link href="tel:51920215">
-                            <a className="calla" style={{ backgroundColor: "#3B712E" }}>AppelNous</a>
-                        </Link>
-                    </div>
+            <nav className={styles.navbar} >
+                <div className={styles.logo}>
+                    <Link href="/">SBINFORMATIQUES</Link>
+                </div>
+                <div className={styles.resbtn}>
+                    <button onClick={() => {
+                        setNav(Nav === "none" ? "block" : "none")
+                    }}>
+                        <Bars />
+                    </button>
+                </div>
+                <nav className={styles.nav} style={{ display: Nav }} >
+                    <Link href="/">Acceuil</Link>
+                    <Link href="/services">Services</Link>
+                    <Link href="/about">A Propos</Link>
                 </nav>
-            </header>
+                <div className={styles.contact} style={{ display: Nav }}>
+                    <Link href="/contact">
+                        <a className="contacta" style={{ backgroundColor: "var(--Blue)" }}>ContactezNous</a>
+                    </Link>
+                    <Link href="tel:21651920215">
+                        <a className="calla" style={{ backgroundColor: "#3B712E" }}>AppelNous</a>
+                    </Link>
+                </div>
+            </nav>
+        </header>
     )
 }
 
